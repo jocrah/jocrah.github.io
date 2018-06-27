96332620da07194abb14418dc195942bed4e531b
Joshua Ocrah <ocrahjoshua@gmail.com>
2018-06-27 12:25:25 +0000

    Populated Select Element with FreeCurrencyConverterAPI with FetchAPI

--- js/index.js
+++ js/index.js
@@ -9,3 +9,36 @@ if ('serviceWorker' in navigator) {
       console.log('Registration failed with ' + error);
     });
   }
+
+  const url = "https://free.currencyconverterapi.com/api/v5/currencies";
+
+  function createNode(element){
+    return document.createElement(element);
+  }
+
+  function append(parent, el){
+    return parent.appendChild(el);
+  }
+
+  window.onload= function(){
+    const fromSelect = document.getElementById('from-select');
+    const toSelect = document.getElementById('to-select');
+    fetch(url)
+    .then((response)=>response.json())
+    .then(function(data){
+      let currencies = data.results;
+      for(key in currencies){
+        let fromOption = createNode('option'),
+            toOption = createNode('option');
+        
+        fromOption.innerHTML = `${currencies[key].currencyName}`;
+        toOption.innerHTML = `${currencies[key].currencyName}`;
+        append(fromSelect,fromOption);
+        append(toSelect, toOption);
+      }
+    })
+  .catch(function(error){
+    console.log(error);
+  });
+  };
+ 
