2df800a324eb48d91352b9955940589c0b566bd7
Joshua Ocrah <ocrahjoshua@gmail.com>
2018-06-27 14:58:09 +0000

    Written Minimal version of online-only conversion and UI Change

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
     });
   }
 
-  const url = "https://free.currencyconverterapi.com/api/v5/currencies";
+  const BASE_URL = "https://free.currencyconverterapi.com/api/v5/";
 
   function createNode(element){
     return document.createElement(element);
@@ -23,7 +23,10 @@ if ('serviceWorker' in navigator) {
   window.onload= function(){
     const fromSelect = document.getElementById('from-select');
     const toSelect = document.getElementById('to-select');
-    fetch(url)
+    const inputAmount = document.getElementById('input-amount');
+    const outputAmount = document.getElementById('output-amount');
+
+    fetch(BASE_URL+"currencies")
     .then((response)=>response.json())
     .then(function(data){
       let currencies = data.results;
@@ -32,7 +35,9 @@ if ('serviceWorker' in navigator) {
             toOption = createNode('option');
         
         fromOption.innerHTML = `${currencies[key].currencyName}`;
+        fromOption.value = `${key}`;
         toOption.innerHTML = `${currencies[key].currencyName}`;
+        toOption.value = `${key}`;
         append(fromSelect,fromOption);
         append(toSelect, toOption);
       }
@@ -40,5 +45,39 @@ if ('serviceWorker' in navigator) {
   .catch(function(error){
     console.log(error);
   });
+
+  let button = this.document.getElementById('convert');
+  button.addEventListener('click', function(){
+    
+
+    let convParameters='';
+    //build parameters
+    let amount = parseFloat(inputAmount.value);
+    
+    let inputSelect = fromSelect.options[fromSelect.selectedIndex].value;
+    console.log(inputSelect);
+    let outputSelect = toSelect.options[toSelect.selectedIndex].value;
+    let valid = (inputSelect !=='' && outputSelect!==''&&inputSelect!==null && outputSelect!==null);
+    console.log(valid);
+    if(valid==true){
+      convParameters = `convert?q=${inputSelect}_${outputSelect}`;
+      fetch(BASE_URL+convParameters)
+      .then((response)=>response.json())
+      .then(function(data){
+        let conv = data.results;
+        if(!isNaN(amount)){
+          let new_amount = amount*conv[`${inputSelect}_${outputSelect}`].val;
+          outputAmount.value = new_amount;
+        }else{
+          console.log('No amount specified or NaN');
+        }
+        
+      })
+    .catch(function(error){
+      console.log(error);
+    });
+    }
+  });
+
   };
  
